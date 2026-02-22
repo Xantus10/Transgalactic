508e0635995100c17be6821835e908d40a91296e
xantus10 <jaroslav.zaba@post.cz>
2026-02-22 17:33:21 +0100

    Global state

---
+++ tgl/globals.py
@@ -0,0 +1,27 @@
+from uuid import uuid4
+
+from .errors import TGLIdentifierError
+
+safeuuid = lambda: str(uuid4()).replace('-', '_')
+
+class Global:
+  _Prefix = 'TGL__' + safeuuid() + '_'
+  _UsedLabels: list[str] = []
+
+  @staticmethod
+  def overridePrefix(new_prefix: str):
+    Global._Prefix = new_prefix
+
+  @staticmethod
+  def getGlobalIdFor(name: str):
+    if name in Global._UsedLabels: raise TGLIdentifierError(f'Duplicit identifier \'{name}\'', name)
+    Global._UsedLabels.append(name)
+    return Global._Prefix + name
+  
+  @staticmethod
+  def getLocalIdFor(name: str):
+    return '.' + Global._Prefix + name
+  
+  @staticmethod
+  def getRandId():
+    return Global.getGlobalIdFor(safeuuid())
