98be9b34360c7b30b6f43e0006df2cfce5a3e881
xantus10 <jaroslav.zaba@post.cz>
2026-02-22 17:34:22 +0100

    Error handling

--- tgl/interpreter.py
+++ tgl/interpreter.py
@@ -1,3 +1,4 @@
+from .errors import TGLIdentifierError
 from .types import InstructionList, ModuleTree, TGLLine
 
 from .modules.mc import FUNCTIONS as MC_FUNCTIONS
@@ -8,4 +9,5 @@ MODULE_TREE: ModuleTree = {
 }
 
 def interpret(parsed_line: TGLLine) -> InstructionList:
+  if not parsed_line['func'] in MODULE_TREE[parsed_line['module']].keys(): raise TGLIdentifierError(f'Module \'{parsed_line['module']}\' does not expose function \'{parsed_line['func']}\'', str(parsed_line))
   return MODULE_TREE[parsed_line['module']][parsed_line['func']](parsed_line['args'])
