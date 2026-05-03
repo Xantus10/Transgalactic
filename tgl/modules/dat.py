008162e51a96ba696117252ca747327be2db5708
xantus10 <jaroslav.zaba@post.cz>
2026-05-03 13:51:12 +0200

    dat module init function

--- tgl/modules/dat.py
+++ tgl/modules/dat.py
@@ -13,6 +13,10 @@ def init(args: list[TypedArgument]) -> InstructionList:
     if not isArgInt(args[0]): raise TGLArgumentError.preset({'et': 'argtypes', 'func_name': 'init', 'expected': ('int',), 'got': (args[0]['argtype'],)}, str(args))
     pagesize = args[0]['value'] * 4096
 
+  rax = Global.regs['r1']['b64']
+  rdi = Global.regs['r2']['b64']
+  rsi = Global.regs['r3']['b64']
+  rdx = Global.regs['r4']['b64']
 
   alChunkLabel, _ = Global.getGlobalIdFor('AllocChunk')
   frChunkLabel, _ = Global.getGlobalIdFor('FreeChunk')
@@ -35,7 +39,43 @@ def init(args: list[TypedArgument]) -> InstructionList:
     {
       'op': 'section',
       'section': '.data',
-      'content': []
+      'content': [
+        f'struc {alChunkLabel}', # AllocChunk struc
+        '.prev_size resq 1',
+        '.size resq 1',
+        'endstruc',
+        f'struc {frChunkLabel}', # FreeChunk struc
+        '.prev_size resq 1',
+        '.size resq 1',
+        '.prev resq 1',
+        '.next resq 1',
+        'endstruc',
+        f'{pagesizeLabel} equ {pagesize}', # ASM constants
+        f'{flagsSpaceLabel} equ 15',
+        f'{flagsSpaceInvLabel} equ -16',
+        f'{flagPrevinuseLabel} equ 1',
+        f'{flagPrevinuseInvLabel} equ -2',
+        f'{headLabel}: dq 0', # Global variables
+        f'{curLabel}: dq 0',
+        f'{freeheadLabel}: dq 0'
+      ]
+    },
+    {
+      'op': 'section',
+      'section': '.text',
+      'content': [
+        f'{initLabel}:',
+        f'! mc mmap PAGE_SIZE',
+        f'mov qword [rel {headLabel}], rax',
+        f'mov qword [rel {curLabel}], rax',
+        'ret'
+      ]
+    },
+    {
+      'op': None,
+      'content': [
+        f'call {initLabel}'
+      ]
     }
   ]
 
